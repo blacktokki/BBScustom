6a498d98688f328498508267e1db9952e5004c9b
namuwiky <39031723+namuwiky@users.noreply.github.com>
2019-02-18 19:48:15 +0900

    컨트롤러 url 출력

--- BBScustom/src/main/java/com/mycompany/BBScustom/HomeController.java
+++ BBScustom/src/main/java/com/mycompany/BBScustom/HomeController.java
@@ -4,12 +4,17 @@ import java.text.DateFormat;
 import java.util.Date;
 import java.util.Locale;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+//import org.springframework.web.bind.annotation.RequestAttribute;
+import org.springframework.web.servlet.NoHandlerFoundException;
 
 /**
  * Handles requests for the application home page.
@@ -21,14 +26,14 @@ public class HomeController {
 	
 	/**
 	 * Simply selects the home view to render by returning its name.
-	 */
+	 */	
+	//@RequestAttribute("javax.servlet.forward.request_uri")
 	@RequestMapping(value = "/", method = RequestMethod.GET)
-	public String home(Locale locale, Model model) {
+	public String home(Locale locale, Model model)
+	{
 		logger.info("Welcome home! The client locale is {}.", locale);
-		
 		Date date = new Date();
 		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
-		
 		String formattedDate = dateFormat.format(date);
 		
 		model.addAttribute("serverTime", formattedDate );
@@ -36,4 +41,15 @@ public class HomeController {
 		return "home";
 	}
 	
+	@RequestMapping(value = {"/*"}, method = RequestMethod.GET)
+	public String home(HttpServletRequest request)
+	{
+		String str=request.getRequestURI();
+		logger.info("request.getServletPath is {}.", str);
+		return "redirect:/";
+	}
+	@ExceptionHandler(NoHandlerFoundException.class)
+    public String handle(Exception ex) {
+       return "redirect:/";//this is view name
+   }
 }
