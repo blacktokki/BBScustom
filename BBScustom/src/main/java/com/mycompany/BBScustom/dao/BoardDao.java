4388795aa8322314324742ad0d97275e6d3d39d2
namuwiky <39031723+namuwiky@users.noreply.github.com>
2019-02-23 22:22:08 +0900

    Dao객체 생성자 추가

--- BBScustom/src/main/java/com/mycompany/BBScustom/dao/BoardDao.java
+++ BBScustom/src/main/java/com/mycompany/BBScustom/dao/BoardDao.java
@@ -1,10 +1,25 @@
 package com.mycompany.BBScustom.dao;
 
 import java.util.ArrayList;
+
+import javax.naming.*;
+import javax.sql.DataSource;
+
 import com.mycompany.BBScustom.dto.BoardDto;
 
 public class BoardDao {
 	
+	DataSource dataSource;
+	
+	public BoardDao(){
+		try {
+			Context context =new InitialContext();
+			dataSource =(DataSource)context.lookup("");
+		}
+		catch(NamingException e) {
+			e.printStackTrace();
+		}
+	}
 	
 	public ArrayList<BoardDto> list(){
 		ArrayList<BoardDto> dtos=null;
