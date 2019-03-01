c0984d721bb6fc28725cceb1df8502760920e782
namuwiky <39031723+namuwiky@users.noreply.github.com>
2019-03-01 23:09:46 +0900

    dataSource 트랜잭션기능 추가

--- BBScustom/src/main/java/com/mycompany/BBScustom/controller/BoardController.java
+++ BBScustom/src/main/java/com/mycompany/BBScustom/controller/BoardController.java
@@ -7,8 +7,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.*;
 
 import com.mycompany.BBScustom.service.*;
 /**
@@ -111,5 +110,4 @@ public class BoardController {
 		service.excute(model);
 		return  "redirect:list";
 	}
-	
 }
--- BBScustom/src/main/java/com/mycompany/BBScustom/dao/AbstractDao.java
+++ BBScustom/src/main/java/com/mycompany/BBScustom/dao/AbstractDao.java
@@ -4,17 +4,24 @@ import java.sql.*;
 import javax.naming.*;
 import javax.sql.DataSource;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.TransactionDefinition;
+import org.springframework.transaction.TransactionStatus;
+import org.springframework.transaction.support.DefaultTransactionDefinition;
 
 public abstract class AbstractDao {
 
 	protected DataSource dataSource;
 	protected BeanPropertyRowMapper<?> bprm;
+	protected PlatformTransactionManager transactionManager;
 	
 	protected <T> void initialize(String name,Class<T> mappedClass) {
 		try {
 			Context context =new InitialContext();
 			dataSource =(DataSource)context.lookup(name);
 			bprm=new BeanPropertyRowMapper<>(mappedClass);
+			transactionManager=new DataSourceTransactionManager(dataSource);
 		}
 		catch(NamingException e) {
 			e.printStackTrace();
@@ -25,12 +32,16 @@ public abstract class AbstractDao {
 		Connection connection=null;
 		PreparedStatement preparedStatement=null;
 		ResultSet resultSet=null;
+		TransactionDefinition definition= new DefaultTransactionDefinition();
+		TransactionStatus status=transactionManager.getTransaction(definition);
 		try {
 			connection = dataSource.getConnection();
 			preparedStatement=connection.prepareStatement(query);
 			resultSet=daoquery.executeTry(preparedStatement,resultSet);
+			transactionManager.commit(status);
 		}
 		catch(Exception e) {
+			transactionManager.rollback(status);
 			e.printStackTrace();
 		}
 		finally {
