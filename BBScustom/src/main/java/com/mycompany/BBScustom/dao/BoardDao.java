966fb33269b7c7db9e240fa985233cd2bd3c38a9
namuwiky <39031723+namuwiky@users.noreply.github.com>
2019-03-10 00:22:16 +0900

    Dao에 jdbctemplate적용,네이밍 리팩토링

--- BBScustom/src/main/java/com/mycompany/BBScustom/dao/BoardDao.java
+++ BBScustom/src/main/java/com/mycompany/BBScustom/dao/BoardDao.java
@@ -1,120 +1,26 @@
 package com.mycompany.BBScustom.dao;
 
-import java.sql.*;
 import java.util.ArrayList;
 import com.mycompany.BBScustom.dto.BoardDto;
 
-import org.springframework.stereotype.Repository;
-
-@Repository
-public class BoardDao extends AbstractDao implements IBoardDao{
+public interface BoardDao extends Dao{
 	
-	public BoardDto contentView(String idx) {
-		ArrayList<BoardDto> dtos=new ArrayList<>();
-		execute("SELECT * FROM board where idx=?",new DaoException(){
-			public ResultSet executeTry(PreparedStatement ps, ResultSet rs) throws SQLException{
-				ps.setInt(1,Integer.parseInt(idx));
-				rs =ps.executeQuery();
-				mapper(BoardDto.class, rs, dtos);
-				return rs;
-			}
-		});
-		return dtos.get(0);
-	}
+	public BoardDto contentView(String idx);
 	
-	public BoardDto replyView(String idx) {
-		ArrayList<BoardDto> dtos = new ArrayList<>();
-		execute("SELECT * FROM board where idx=?",new DaoException(){
-			public ResultSet executeTry(PreparedStatement ps, ResultSet rs) throws SQLException{
-				ps.setInt(1,Integer.parseInt(idx));
-				rs =ps.executeQuery();
-				mapper(BoardDto.class, rs, dtos);
-				return rs;
-			}
-		});
-		return dtos.get(0);
-	}
+	public BoardDto replyView(String idx);
 	
-	public ArrayList<BoardDto> list(){
-		ArrayList<BoardDto> dtos=new ArrayList<BoardDto>();
-		execute("SELECT idx, title, content, bdname, bddate, bdgroup, step, indent, hit FROM board order by bdgroup desc,step asc",new DaoException(){
-			public ResultSet executeTry(PreparedStatement ps, ResultSet rs) throws SQLException{
-				rs =ps.executeQuery();
-				mapper(BoardDto.class, rs, dtos);
-				return rs;
-			}
-		});
-		return (ArrayList<BoardDto>) dtos;
-	}
+	public ArrayList<BoardDto> list();
 	
-	public void write(String bdname,String title,String content) {
-		execute("insert into board (title,content,bdname,bdgroup,step,indent) select ?,?,?, ifnull(max(idx),0)+1 ,0,0 from board;",new DaoException(){
-			public ResultSet executeTry(PreparedStatement ps, ResultSet rs) throws SQLException{
-				ps.setString(1,title);
-				ps.setString(2,content);
-				ps.setString(3,bdname);	
-				ps.executeUpdate();
-				return rs;
-			}
-		});
-	}
+	public void write(String bdname,String title,String content);
 	
-	public void modify(String idx,String bdname,String title,String content) {
-		execute("update board set bdname = ?,title = ?,content =?  where idx= ? ",new DaoException(){
-			public ResultSet executeTry(PreparedStatement ps, ResultSet rs) throws SQLException{
-				ps.setString(1,bdname);
-				ps.setString(2,title);
-				ps.setString(3,content);
-				ps.setInt(4,Integer.parseInt(idx));			
-				ps.executeUpdate();
-				return rs;
-			}
-		});
-	}
+	public void modify(String idx,String bdname,String title,String content);
 	
-	public void delete(String idx) {
-		execute("delete from board where idx= ? ",new DaoException(){
-			public ResultSet executeTry(PreparedStatement ps, ResultSet rs) throws SQLException{
-				ps.setInt(1,Integer.parseInt(idx));			
-				ps.executeUpdate();
-				return rs;
-			}
-		});
-	}
+	public void delete(String idx);
 	
-	public void reply(String idx,String bdname,String title,String content,String bdgroup, String step, String indent){
-		execute("insert into board (title,content,bdname,bdgroup,step,indent) values (?,?,?,?,?,?);",new DaoException(){
-			public ResultSet executeTry(PreparedStatement ps, ResultSet rs) throws SQLException{
-				ps.setString(1,title);
-				ps.setString(2,content);
-				ps.setString(3,bdname);	
-				ps.setInt(4,Integer.parseInt(bdgroup));	
-				ps.setInt(5,Integer.parseInt(step)+1);	
-				ps.setInt(6,Integer.parseInt(indent)+1);	
-				ps.executeUpdate();
-				return rs;
-			}
-		});
-	}
-	public void replyShape(String bdgroup,String step){
-		execute("update board set step = step+1 where bdgroup = ? and step > ?",new DaoException(){
-			public ResultSet executeTry(PreparedStatement ps, ResultSet rs) throws SQLException{
-				ps.setInt(1,Integer.parseInt(bdgroup));	
-				ps.setInt(2,Integer.parseInt(step));		
-				ps.executeUpdate();
-				return rs;
-			}
-		});
-	}
+	public void reply(String bdname,String title,String content,String bdgroup, String step, String indent);
 	
-	public void upHit(String idx) {
-		execute("update board set hit = hit+1 where idx= ? ",new DaoException(){
-			public ResultSet executeTry(PreparedStatement ps, ResultSet rs) throws SQLException{
-				ps.setString(1,idx);			
-				ps.executeUpdate();
-				return rs;
-			}
-		});
-	}
+	public void replyShape(String bdgroup,String step);
+	
+	public void upHit(String idx);
 
 }
