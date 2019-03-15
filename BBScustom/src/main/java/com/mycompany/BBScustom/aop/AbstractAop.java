1eb7ec6036b97d9620bf969a0499efcc7a13e61c
namuwiky <39031723+namuwiky@users.noreply.github.com>
2019-03-15 23:36:40 +0900

    트랜잭션 AOP추가

---
+++ BBScustom/src/main/java/com/mycompany/BBScustom/aop/AbstractAop.java
@@ -0,0 +1,9 @@
+package com.mycompany.BBScustom.aop;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class AbstractAop {
+	protected static final Logger logger = LoggerFactory.getLogger(AbstractAop.class);
+	protected static String indent="";
+}
